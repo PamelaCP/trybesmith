e42d02f0a93ee1188d7b03c0bb019b070a1df42e
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-14 19:18:26 -0300

    corrigindo insertId model Product

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,4 +1,3 @@
-import { create } from "domain";
 import { ResultSetHeader, Pool } from "mysql2/promise";
 import Product from '../interfaces/product.interface';
 
@@ -8,17 +7,15 @@ class ProductsModel {
     constructor(connection: Pool) {
         this.connection = connection;
     }
-
 // Requisito 01 Cadastro Produto 
-
     public async create(product: Product): Promise<Product> {
         const { name, amount } = product;
         const result = await this.connection.execute<ResultSetHeader>('INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
         [name, amount],
         );
         const [dataInserted] = result;
-        const { insetId } = dataInserted;
-        return { id: insetId, ...product};
+        const { insertId } = dataInserted;
+        return { id: insertId, ...product };
     }
     
 }
