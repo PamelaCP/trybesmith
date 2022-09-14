aa6f797e4b6f11fbb121adbb56ce0ede528f2fd2
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-14 17:36:36 -0300

    iniciando o arquivo model Product

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,2 +1,13 @@
+import { ResultSetHeader } from "mysql2";
+import Pool from "mysql2/typings/mysql/lib/Pool";
+
+
+export default class ProductModel {
+    public connection: Pool;
+
+    constructor(connection: Pool) {
+        this.connection = connection;
+    }
+}
 
 
